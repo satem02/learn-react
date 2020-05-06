5a3c3529bd0852c495581af1032e52778d62249c
Şafak Temel <safaktemel02@gmail.com>
2020-05-06 10:35:48 +0300

    Add semantic package

--- learn-react/src/components/root/index.js
+++ learn-react/src/components/root/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+// Functional Component orneği.Birde Class componentler var.
 const Root =  () => <div> Hello </div>;
 
 export default Root;
--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// bu şekilde default export edilen kullanılır.
+// {Root} bu sekilde birden fazla export olabilir.
 import Root from './components/root'
 
 
